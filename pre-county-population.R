e78ac6a82474673fb2ee7102ac86a71152e84311
wwang93 <wwang93@vols.utk.edu>
2025-01-26 16:28:03 -0500

    update the predata (fake demand)

--- pre-county-population.R
+++ pre-county-population.R
@@ -1,18 +1,23 @@
+
+### use county population as "demand" data
+library(sf)
+library(urbnmapr)
+library(dplyr)
+library(tidyverse)
+library(haven)
 library(tidycensus)
 
 county_population <- get_acs(
   geography = "county",
-  variables = "B01003_001E", # 总人口变量
+  variables = "B01003_001E", # Total population variable
   year = 2021,
   survey = "acs5"           # 5年期数据
 )
 
 head(county_population)
 
-
-### fake demand data(county population data)
 library(dplyr)
-# 选择county_population中的变量Geo_ID和Name和estimate
+# select  Geo_ID;Nameand estimate
 counties_population <- county_population %>%
   select(GEOID, NAME, estimate) %>%
   rename(county_fips = GEOID, county_name = NAME, population = estimate)
@@ -22,7 +27,7 @@ library(sf)
 library(urbnmapr)
 counties_sf1 <- get_urbn_map("counties", sf = TRUE)
 
-# 将counties_sf和counties_population合并
+# join counties_sf and counties_population
 library(tidyverse)
 counties_sf_size <- counties_sf1 %>%
   left_join(counties_population, by = c("county_fips" = "county_fips"))
@@ -31,14 +36,13 @@ counties_sf_size1 <- counties_sf_size %>%
   select(county_fips, county_name.y, population, geometry)%>%
   rename(county_name = county_name.y)
 
-# couties_sf_size是fake demand data
 
 counties_sf_size2 <- st_transform(counties_sf_size1, crs = 4326)
 
 
-library(sf)
-library(urbnmapr)
-library(mapboxer)
+
+### igore these lines, we don't use mapboxer
+#library(mapboxer)
 #counties_sf_size2 <- counties_sf_size2 %>% 
   #as_mapbox_source()
 
@@ -60,11 +64,7 @@ ipeds_green_summed <- ipeds_green_summed %>%
 
 write_rds(ipeds_green_summed, "ipeds_green_summed.rds")
 
-library(sf)
-library(urbnmapr)
-library(dplyr)
-library(tidyverse)
-library(haven)
+
 
 hdallyears <- read_dta("raw-data/hdallyears.dta")
 
